48e96c26082ecc419a807706cf705482e7b03309
EDebbuger <locktermac@gmail.com>
2026-06-06 14:38:03 +0200

    'first commit to track remote'

---
+++ src/test/java/com/EDebugger/booking_logic/BookingLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EDebugger.booking_logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
